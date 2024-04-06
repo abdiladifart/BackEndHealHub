cc8e53a7dcc35046531f3814081f0dfdc6923d39
Abdiladif <abdiladifart1996@gmail.com>
2024-04-07 01:22:38 +0300

    Initial commit

---
+++ src/main/java/com/example/backendhealhub/BackEndHealHubApplication.java
@@ -0,0 +1,13 @@
+package com.example.backendhealhub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackEndHealHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackEndHealHubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/backendhealhub/BackEndHealHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.backendhealhub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackEndHealHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
